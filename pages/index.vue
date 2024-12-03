412c133361f7f2c529231d8933c944628d26e964
Santosh Kumar Patro <santoshkpatro11@gmail.com>
2024-12-04 00:43:12 +0530

    updated email.

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,5 @@
 <template>
-  <main
-    class="flex flex-col justify-center items-center text-center p-6"
-  >
+  <main class="flex flex-col justify-center items-center text-center p-6">
     <div class="max-w-4xl">
       <!-- Big Heading -->
       <h1 class="text-5xl font-bold text-gray-800 mb-2">
@@ -11,10 +9,10 @@
       <!-- Current Position -->
       <p class="text-2xl text-gray-700 mb-4">
         I’m a
-        <span class="font-semibold text-gray-900 jazz"
-          >Senior Full Stack Engineer</span
-        >
-        at <span class="font-semibold text-blue-600 jazz">AcornGlobus</span>, where I
+        <span class="font-semibold text-gray-900 jazz">Senior Full Stack Engineer</span>
+        at <span class="font-semibold text-blue-600 jazz cursor-pointer">
+          <NuxtLink to="https://acornglobus.com">AcornGlobus</NuxtLink>
+        </span>, where I
         craft everything from
         <span class="font-semibold text-gray-800 jazz">robust backends</span> to
         <span class="font-semibold text-gray-800 jazz">jazzy frontends</span>.
@@ -31,23 +29,24 @@
       </p>
 
       <p class="text-2xl text-gray-700 leading-relaxed mt-2">
-        I’ve also built <span class="font-semibold jazz">Shopify apps</span>, smashed birdies on the <span class="font-semibold jazz">badminton court 🏸</span>,
+        I’ve also built <span class="font-semibold jazz">Shopify apps</span>, smashed birdies on the <span
+          class="font-semibold jazz">badminton court 🏸</span>,
         and am actively working on two side projects:
       </p>
 
       <div class="mt-4">
         <ul class="list-none">
           <li class="text-2xl text-blue-600 font-semibold">
-            Taskite:
-            <span class="text-gray-700"
-              >An open-source project management tool.</span
-            >
+            <span class="jazz cursor-pointer">
+              <NuxtLink to="https://github.com/santoshkpatro/taskite">Taskite</NuxtLink>
+            </span>:
+            <span class="text-gray-700">An open-source project management tool.</span>
           </li>
           <li class="text-2xl text-blue-600 font-semibold mt-2">
-            Calyvim:
-            <span class="text-gray-700"
-              >An invoicing and bookkeeping tool.</span
-            >
+            <span class="jazz cursor-pointer">
+              <NuxtLink to="https://github.com/santoshkpatro/calyvim">Calyvim</NuxtLink>
+            </span>:
+            <span class="text-gray-700">An invoicing and bookkeeping tool.</span>
           </li>
         </ul>
       </div>
@@ -61,13 +60,9 @@
       </p>
 
       <p class="text-2xl text-gray-700 leading-relaxed mt-2">
-        Feel free to reach out on Twitter or email me at
-        <a
-          href="mailto:santoshkpatro11@gmail.com"
-          class="text-blue-600 underline"
-        >
-          santoshkpatro11@gmail.com </a
-        >.
+        Feel free to reach out on X or email me at
+        <a href="mailto:santoshkpatro11@gmail.com" class="text-blue-600 underline">
+          hey@santoshkpatro.in</a>.
       </p>
 
       <!-- Caption -->
@@ -86,11 +81,13 @@
 }
 
 @keyframes wave-animation {
+
   0%,
   60%,
   100% {
     transform: rotate(0deg);
   }
+
   30% {
     transform: rotate(-10deg);
   }
@@ -98,7 +95,9 @@
 
 /* Hover effect for bold text */
 .jazz:hover {
-  background-color: #8B5CF6; /* Primary color */
-  color: white; /* Text color becomes white */
+  background-color: #8B5CF6;
+  /* Primary color */
+  color: white;
+  /* Text color becomes white */
 }
 </style>
